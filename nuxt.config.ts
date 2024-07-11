c658dd4034a2ba6b77d813f3b36d1d347fd5aca5
oleggladkii <oleg.gladkii@onix-systems.com>
2024-07-11 14:04:07 +0300

    Updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/nuxt-github-pages/',
+    buildAssetsDir: 'assets'
+  },
   devtools: { enabled: true },
   modules: ['nuxt-quasar-ui', '@nuxt/eslint'],
   quasar: {
